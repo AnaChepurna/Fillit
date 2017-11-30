686c7aae0164fcaa8cc7d92f010f4f287d3bdeaf
AnaChepurna <ch.nastasie@gmail.com>
2017-11-30 13:21:26 +0200

    perfect

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -16,4 +16,19 @@ typedef struct	s_tetrimino
 	char	c;
 }		t_tetrimino;
 
+/*solve.c*/
+int		solve(t_tetrimino **tetri);
+void		start(t_tetrimino **tetri);
+
+/*tetrimino_manipulate.c*/
+int		check_position(t_tetrimino *tetri, int y, int x);
+void		add_tetri(t_tetrimino *tetri,int y, int x);
+void		remove_tetri(t_tetrimino *tetri, int y, int x);
+void		print_tetri(t_tetrimino *tetri);
+
+/*map_manipulate.c*/
+int		get_mapsize(t_tetrimino **tetri);
+char		**init_map(void);
+void		print_map(void);
+
 #endif
